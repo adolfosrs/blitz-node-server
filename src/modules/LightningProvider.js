787992d5339ece6d57cf1eedfd192246b67a4e3a
adolfosrs <adolfosrs@gmail.com>
2020-02-06 13:15:29 -0300

    Use mainnet macaroon

--- src/modules/LightningProvider.js
+++ src/modules/LightningProvider.js
@@ -5,7 +5,7 @@ process.env.GRPC_SSL_CIPHER_SUITES = 'HIGH+ECDSA'
 const lndCert = fs.readFileSync('/home/bitcoin/.lnd/tls.cert');
 const sslCreds = grpc.credentials.createSsl(lndCert);
 const macaroonCreds = grpc.credentials.createFromMetadataGenerator((args, callback) => {
-  const macaroon = fs.readFileSync('/home/bitcoin/.lnd/data/chain/bitcoin/simnet/admin.macaroon').toString('hex');
+  const macaroon = fs.readFileSync('/home/bitcoin/.lnd/data/chain/bitcoin/mainnet/admin.macaroon').toString('hex');
   const metadata = new grpc.Metadata()
   metadata.add('macaroon', macaroon);
   callback(null, metadata);
