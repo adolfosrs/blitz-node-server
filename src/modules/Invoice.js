50b13982cfbbb41cef2c44514fe39d41f666ef5f
adolfosrs <adolfosrs@gmail.com>
2020-02-11 20:40:59 -0300

    Add missing parameters to model addInvoice request body

--- src/modules/Invoice.js
+++ src/modules/Invoice.js
@@ -6,28 +6,35 @@ const lightning = LightningProvider.lightning;
 function createInvoice(params) {
   return new Promise((resolve, reject) => {
     const request = {
-      memo: 'tetet',
-      // r_preimage: < bytes > ,
-      // r_hash: < bytes > ,
+      memo: params.memo,
       value: params.amt,
-      // value_msat: < int64 > ,
-      // settled: < bool > ,
-      // creation_date: < int64 > ,
-      // settle_date: < int64 > ,
-      // payment_request: < string > ,
-      // description_hash: < bytes > ,
-      // expiry: < int64 > ,
-      // fallback_addr: < string > ,
-      // cltv_expiry: < uint64 > ,
-      // route_hints: < array RouteHint > ,
-      // state: < InvoiceState > ,
-      // htlcs: < array InvoiceHTLC > ,
-      // features: < array FeaturesEntry > ,
+      // r_preimage: <bytes>,
+      // r_hash: <bytes>,
+      // value_msat: <int64>,
+      // settled: <bool>,
+      // creation_date: <int64>,
+      // settle_date: <int64>,
+      // payment_request: <string>,
+      // description_hash: <bytes>,
+      // expiry: <int64>,
+      // fallback_addr: <string>,
+      // cltv_expiry: <uint64>,
+      // route_hints: <array RouteHint>,
+      // private: <bool>,
+      // add_index: <uint64>,
+      // settle_index: <uint64>,
+      // amt_paid: <int64>,
+      // amt_paid_sat: <int64>,
+      // amt_paid_msat: <int64>,
+      // state: <InvoiceState>,
+      // htlcs: <array InvoiceHTLC>,
+      // features: <array FeaturesEntry>,
+      // is_keysend: <bool>,
     };
 
     lightning.addInvoice(request, (err, response) => {
       console.log('addInvoice', response, err);
-      return resolve(response.payment_request);
+      return resolve(response);
     });
   });
 }
