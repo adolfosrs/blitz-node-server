567924d2561d97971dcfa6810b41a36ecbff4625
adolfosrs <adolfosrs@gmail.com>
2020-02-11 20:40:11 -0300

    Remove boiler plate logic

--- src/controllers/invoice.js
+++ src/controllers/invoice.js
@@ -3,23 +3,10 @@
 const InvoiceModule = require('../modules/Invoice.js');
 
 function createInvoice(req, res) {
-  const body = req.body;
-  const message = body.message;
-  const author = body.author;
-  const link = body.link;
-  const uid = body.uid;
-  const amt = body.amt;
+  const params = req.body;
 
-  console.log('createInvoice', body);
-
-  if (!message || !uid) {
-    return res.json('Missing parameters');
-  }
-
-  InvoiceModule.createInvoice(uid, amt, message, author, link).then(invoice => {
-    return res.json({
-      pay_req: invoice.pay_req
-    });
+  InvoiceModule.createInvoice(params).then(createdInvoice => {
+    return res.json(createdInvoice);
   }).catch(errorMessage => res.json(errorMessage));
 }
 
