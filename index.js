cd31c8e0dd52d3fb5101c9cc80402d0c565ea4f7
adolfosrs <adolfosrs@gmail.com>
2020-02-11 21:15:41 -0300

    Add express-rate-limit to help prevent DDOS attacks

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 'use strict';
 
-const http = require('http')
+const http = require('http');
 const express = require('express');
+const RateLimit = require('express-rate-limit');
+
+const apiLimiter = new RateLimit({
+  windowMs: 60 * 1000, // 1 minutes
+  max: 10,
+});
 
 const app = express();
 
+app.use('/', apiLimiter);
 app.use('/v0', require('./src/routes.js'));
 
 const port = process.env.PORT || 8099;
